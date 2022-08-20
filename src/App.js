91c7918aaeb347c327dc69ddf60e00d9b1cc0245
Alimi Faith <73644238+ftoucch@users.noreply.github.com>
2022-08-20 09:17:52 +0100

    Complete Code

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+  const [index, setIndex] = useState(0);
+  const { id, image, name, title, quote } = data[index];
+
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        return checkNumber(newIndex);
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, []);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <article>
+          <img src={image} alt={name} className="person-img" />
+          <h4>{name}</h4>
+          <p className="title">{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className="icon" />
+        </article>
+
+        <button className="prev" onClick={prevPerson}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextPerson}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
